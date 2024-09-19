5bdee60eac811399d2411f944ba2474e3beebaa8
Mario Bruestle <mario.bruestle@pm.me>
2024-09-19 09:58:49 +0200

    fix

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -324,7 +324,7 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
       local servers = {
-        tsserver = {},
+        ts_ls = {},
         zls = {},
         hls = {},
         svelte = {},
