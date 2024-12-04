e36f803278f198bab82157c4d36b8d171c574b69
Mario Bruestle <mario.bruestle@pm.me>
2024-12-04 21:02:22 +0100

    ha?

--- configs/fish/functions/gptify.fish
+++ configs/fish/functions/gptify.fish
@@ -1,6 +1,7 @@
 function gptify
     for file in (fd -t f)
-        echo $file
+        echo "<file><name>$file</name><content>"
         /usr/bin/cat $file
+        echo "</content></file>"
     end
 end
--- configs/fish/functions/mountnas.fish
+++ configs/fish/functions/mountnas.fish
@@ -1,3 +1,3 @@
 function mountnas
-	sudo mount //192.168.0.138/t7 ~/mnt -o username=nadanke,uid=1000,gid=1000
+	sudo mount //192.168.0.136/data mnt -o username=nadanke,workgroup=WORKGROUP,uid=nadanke,gid=nadanke
 end
