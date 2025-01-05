b64163f7fa733f63d73cba4c7630951691a78ac3
Mario Bruestle <mario.bruestle@pm.me>
2025-01-05 17:13:08 +0100

    fixes

---
+++ configs/fish/functions/dcm.fish
@@ -0,0 +1,3 @@
+function dcm
+docker compose -f ~/dev/hw/morf/momo/docker/docker-compose-dev.yml $argv
+end
--- configs/fish/functions/yt-dlp.fish
+++ configs/fish/functions/yt-dlp.fish
@@ -1,3 +1,3 @@
-function yt-dlp --description 'alias yt-dlp yt-dlp --cookies-from-browser brave:~/.var/app/com.brave.Browser/'
- command yt-dlp --cookies-from-browser brave+gnomekeyring:~/.var/app/com.brave.Browser/ $argv
+function yt-dlp
+ command yt-dlp --cookies-from-browser brave+gnomekeyring:~/.config/BraveSoftware/ $argv
 end
