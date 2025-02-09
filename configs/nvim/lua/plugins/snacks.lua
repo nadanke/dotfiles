3d19279a5d1982c10e66909712131ee2071003b9
Mario Bruestle <mario.bruestle@pm.me>
2025-02-09 14:30:37 +0100

    jigga

---
+++ configs/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			picker = {},
+		},
+		keys = {
+			{ "<leader><space>", function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
+			{ "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
+			{ "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
+			{ "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
+			{ "<leader>n",       function() Snacks.picker.notifications() end,   desc = "Notification History" },
+			{ "<leader>e",       function() Snacks.explorer() end,               desc = "File Explorer" },
+		}
+	},
+}
