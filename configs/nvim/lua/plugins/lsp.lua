72dcb7c1407eb7ad65d133f363ba81532571fd89
Mario Bruestle <mario.bruestle@pm.me>
2025-02-11 16:18:47 +0100

    oii oiii

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,23 @@ return {
 				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
 				lspconfig[server].setup(config)
 			end
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+					map("gd", ":lua Snacks.picker.lsp_definitions()<CR>", "Go to definition")
+					map("gD", ":lua Snacks.picker.lsp_references()<CR>", "Go to references")
+					map("\"", ":lua Snacks.picker.registers()<CR>", "Registers")
+					map("<leader>ld", ":lua Snacks.picker.diagnostics_buffer()<CR>", "Diagnostics Buffer")
+					map("<leader>rn", ":lua vim.lsp.buf.rename<CR>", "Rename")
+					map("<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", "Code Action")
+				end
+			})
 		end,
+
 	},
 }
