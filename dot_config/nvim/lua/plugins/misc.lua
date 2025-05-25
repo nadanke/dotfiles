720b221c49ff84ccd5b1efb6af4a678bb5d977a1
Mario Bruestle <mario.bruestle@pm.me>
2025-05-25 19:49:28 +0200

    yaya

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -79,4 +79,19 @@ return {
 		"windwp/nvim-ts-autotag",
 		opts = {},
 	},
+	{
+		"mistweaverco/kulala.nvim",
+		keys = {
+			{ "<leader>Rs", desc = "Send request" },
+			{ "<leader>Ra", desc = "Send all requests" },
+			{ "<leader>Rb", desc = "Open scratchpad" },
+		},
+		ft = { "http", "rest" },
+		opts = {
+			-- your configuration comes here
+			global_keymaps = false,
+			global_keymaps_prefix = "<leader>R",
+			kulala_keymaps_prefix = "",
+		},
+	},
 }
