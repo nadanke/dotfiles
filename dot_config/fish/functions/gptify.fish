dc5b8a576fa94d6fb572a159226ef8fad84c0333
Mario Bruestle <mario.bruestle@pm.me>
2025-05-26 08:19:13 +0200

    ai

--- dot_config/fish/functions/gptify.fish
+++ dot_config/fish/functions/gptify.fish
@@ -1,5 +1,9 @@
 function gptify
-    for file in (fd -t f)
+    for file in (fd -t f -E "*.lock" -E "package-lock.json" -E "pnpm-lock.yaml" -E "*.png" -E "*.jpg" -E "*.jpeg" -E "*.svg" -E "*.webp" -E "*.ico" -E "*.gif" -E "*.ttf" -E "*.woff" -E "*.woff2" -E "*.otf" -E "*.pdf" -E "*.mp4" -E "*.mp3" -E "*.zip" -E "*.tar" -E "*.gz" -E "*.bz2")
+        # skip binary files
+        if file --mime $file | grep -q binary
+            continue
+        end
         echo "<file><name>$file</name><content>"
         /usr/bin/cat $file
         echo "</content></file>"
